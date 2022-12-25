40e73b2759e5c28fbc7b286ac2986b5aad64e2a1
Waseem Akram <66058079+evildevill@users.noreply.github.com>
2022-12-25 16:47:04 +0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://wasiiai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
